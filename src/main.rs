be3878c470586e5f0695ae59288cf80a5ea4c202
Max Carvajal <jcarvajal288@gmail.com>
2024-08-18 12:09:50 -0500

    Snake moves upward

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ mod level_map;
 mod snake;
 #[path = "levels/level1.rs"] mod level1;
 
+use std::time::Duration;
+use bevy::app::FixedUpdate;
 use bevy::DefaultPlugins;
 use bevy::math::Vec2;
-use bevy::prelude::{App, BuildChildren, Camera2dBundle, Commands, default, IntoSystemConfigs, PluginGroup, Query, Res, Startup, Window};
+use bevy::prelude::{App, BuildChildren, Camera2dBundle, Commands, default, IntoSystemConfigs, PluginGroup, Query, Res, Startup, Time, Window};
+use bevy::time::{Fixed};
 use bevy::window::{WindowPlugin, WindowResolution};
 use crate::images::{Images, load_images};
 use crate::level_map::LevelMap;
@@ -19,10 +22,11 @@ fn main() {
             }),
             ..default()
         }))
+        .insert_resource(Time::<Fixed>::from_duration(Duration::from_millis(500)))
         .insert_resource(Images::default())
         .insert_resource(level_map::load_level_1())
         .add_systems(Startup, (load_images, setup).chain())
-        //.add_systems(Update, player_movement_system)
+        .add_systems(FixedUpdate, snake::snake_movement_system)
         .run();
 }
 fn setup(mut commands: Commands, level_map: Res<LevelMap>, images: Res<Images>, windows: Query<&Window>) {
--- src/snake.rs
+++ src/snake.rs
@@ -1,9 +1,10 @@
-use bevy::prelude::{Commands, Component, default, Res, SpriteBundle};
+use bevy::prelude::{Commands, Component, default, Query, Res, SpriteBundle, Transform, Window};
 use bevy::scene::ron::de::Position;
 use bevy::math::Vec2;
 use bevy::hierarchy::BuildChildren;
 use crate::images::Images;
 use crate::{level1, level_map};
+use crate::level_map::transform_from_position;
 
 #[derive(Component)]
 pub struct SnakeHead {
@@ -36,7 +37,7 @@ pub fn spawn_snake(mut commands: &mut Commands, images: &Res<Images>, window_cen
     let snake_head = commands.spawn((
         SpriteBundle {
             texture: images.snake_head.clone(),
-            transform: level_map::transform_from_position(&start, window_center, 1.0),
+            transform: transform_from_position(&start, window_center, 1.0),
             ..default()
         },
         initial_snake_head(level1::STARTING_POSITION)
@@ -45,7 +46,7 @@ pub fn spawn_snake(mut commands: &mut Commands, images: &Res<Images>, window_cen
         return commands.spawn((
             SpriteBundle {
                 texture: images.snake_tail.clone(),
-                transform: level_map::transform_from_position(&segment.position, window_center, 1.0),
+                transform: transform_from_position(&segment.position, window_center, 1.0),
                 ..default()
             },
             segment
@@ -53,4 +54,19 @@ pub fn spawn_snake(mut commands: &mut Commands, images: &Res<Images>, window_cen
     });
 
     commands.entity(snake_head).push_children(&snake_tail_entities);
+}
+
+pub fn snake_movement_system(
+    mut head_query: Query<(&mut SnakeHead, &mut Transform)>,
+    //mut tail_query: Query<(&mut SnakeTail, &mut Transform)>,
+    windows: Query<&Window>
+) {
+    let window_center = Vec2::new(windows.single().resolution.width() / 2., windows.single().resolution.height() / 2.);
+
+    let (mut head, mut head_transform) = head_query.single_mut();
+    let old_head_position = head.position;
+    if old_head_position.col > 0 && old_head_position.line > 0 {
+        head.position = Position { col: old_head_position.col, line: old_head_position.line - 1 };
+        head_transform.translation = transform_from_position(&head.position, window_center, 1.0).translation;
+    }
 }
