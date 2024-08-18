c2d0042b448691af5f294ac8a362680591bf455b
Max Carvajal <jcarvajal288@gmail.com>
2024-08-18 13:20:28 -0500

    Decoupled tail segments from the head

--- src/snake.rs
+++ src/snake.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::{Commands, Component, default, Query, Res, SpriteBundle, Transform, Window};
+use bevy::prelude::{Commands, Component, default, Query, Res, SpriteBundle, Transform, Window, Without};
 use bevy::scene::ron::de::Position;
 use bevy::math::Vec2;
 use bevy::hierarchy::BuildChildren;
@@ -19,29 +19,31 @@ pub fn initial_snake_head(position: Position) -> SnakeHead {
 
 #[derive(Component)]
 pub struct SnakeTail {
-    pub position: Position
+    pub position: Position,
+    pub order: usize
 }
 
-pub fn initial_snake_tail(position: Position) -> SnakeTail {
+pub fn initial_snake_tail(position: Position, order: usize) -> SnakeTail {
     return SnakeTail {
-        position
+        position,
+        order
     }
 }
 
 pub fn spawn_snake(mut commands: &mut Commands, images: &Res<Images>, window_center: Vec2) {
     let start = level1::STARTING_POSITION;
     let snake_tail = [
-        initial_snake_tail(Position { col: start.col, line: start.line + 1 }),
-        initial_snake_tail(Position { col: start.col, line: start.line + 2 }),
+        initial_snake_tail(Position { col: start.col, line: start.line + 1 }, 0),
+        initial_snake_tail(Position { col: start.col, line: start.line + 2 }, 1),
     ];
-    let snake_head = commands.spawn((
+    commands.spawn((
         SpriteBundle {
             texture: images.snake_head.clone(),
             transform: transform_from_position(&start, window_center, 1.0),
             ..default()
         },
         initial_snake_head(level1::STARTING_POSITION)
-    )).id();
+    ));
     let snake_tail_entities = snake_tail.map(|segment| {
         return commands.spawn((
             SpriteBundle {
@@ -52,13 +54,11 @@ pub fn spawn_snake(mut commands: &mut Commands, images: &Res<Images>, window_cen
             segment
         )).id();
     });
-
-    commands.entity(snake_head).push_children(&snake_tail_entities);
 }
 
 pub fn snake_movement_system(
     mut head_query: Query<(&mut SnakeHead, &mut Transform)>,
-    //mut tail_query: Query<(&mut SnakeTail, &mut Transform)>,
+    mut tail_query: Query<(&mut SnakeTail, &mut Transform), Without<SnakeHead>>,
     windows: Query<&Window>
 ) {
     let window_center = Vec2::new(windows.single().resolution.width() / 2., windows.single().resolution.height() / 2.);
@@ -68,5 +68,13 @@ pub fn snake_movement_system(
     if old_head_position.col > 0 && old_head_position.line > 0 {
         head.position = Position { col: old_head_position.col, line: old_head_position.line - 1 };
         head_transform.translation = transform_from_position(&head.position, window_center, 1.0).translation;
+
+        let mut next_position = old_head_position;
+        for mut tail_segment in &mut tail_query {
+            let mut tail = tail_segment.0;
+            let mut tail_transform = tail_segment.1;
+            (tail.position, next_position) = (next_position, tail.position);
+            tail_transform.translation = transform_from_position(&tail.position, window_center, 1.0).translation;
+        }
     }
 }
