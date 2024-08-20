a7cf369913fdef77836c7ce8bd34c26330ce1f18
Max Carvajal <jcarvajal288@gmail.com>
2024-08-19 21:42:34 -0500

    Move the stairs towards the player a bit

--- src/levels.rs
+++ src/levels.rs
@@ -1,17 +1,17 @@
 #![allow(dead_code)]
 use bevy::asset::ron::de::Position;
 
-pub const LEVEL_1_SELECT: Position = Position { line: 3, col: 3 };
-pub const LEVEL_2_SELECT: Position = Position { line: 3, col: 10 };
-pub const LEVEL_3_SELECT: Position = Position { line: 3, col: 17 };
+pub const LEVEL_1_SELECT: Position = Position { line: 6, col: 3 };
+pub const LEVEL_2_SELECT: Position = Position { line: 6, col: 10 };
+pub const LEVEL_3_SELECT: Position = Position { line: 6, col: 17 };
 
 pub const MENU: &str = "#####################\n\
                         #...................#\n\
                         #...................#\n\
+                        #...................#\n\
+                        #...................#\n\
+                        #...................#\n\
                         #..>......>......>..#\n\
-                        #...................#\n\
-                        #...................#\n\
-                        #...................#\n\
                         #...................#\n\
                         #...................#\n\
                         #...................#\n\
@@ -94,6 +94,6 @@ pub const LEVEL_3: &str = "#####################\n\
                            #####################";
 
 pub const STARTING_POSITION: Position = Position {
-    line: 10,
+    line: 15,
     col: 10,
 };
